d39fb7b9a638263f304ea57b96d47b3745ae83b1
Gabriel Dezzani Pitzer <gabrielpitzer@hotmail.com>
2024-04-24 21:14:16 -0300

    alterando branch master

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const form = document.getElementById('form-numeros');
 
 
-function validaNumero() {
+function validaNumero() { 
 var campoA = document.getElementById('campo-a').value;
 var campoB = document.getElementById('campo-b').value;  
-const mensagemSucesso = `Os números digitados: <b>${campoA}</b> e <b>${campoB}</b> são válidos`;
+const mensagemSucesso = `Os números digitados: <b>${campoA}</b> e <b>${campoB}</b> são válidos`; 
 const errorMessage = `Os números digitados: <b>${campoA}</b> e <b>${campoB} </b> não são válidos`;
 
 
-if (campoB <= campoA) {
+if (campoB < campoA) {
 const containerErrorMessage = document.querySelector('.error-message');
 containerErrorMessage.innerHTML = errorMessage
 containerErrorMessage.style.display = 'block';
